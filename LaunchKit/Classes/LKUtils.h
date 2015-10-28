0df48fbe412d65c0a091d091a7ff92a1c32c53bb
Rizwan Sattar <rsattar@gmail.com>
2015-10-28 14:22:20 -0700

    Add LKUtils, with smart window size getter for now

---
+++ LaunchKit/Classes/LKUtils.h
@@ -0,0 +1,15 @@
+//
+//  LKUtils.h
+//  Pods
+//
+//  Created by Rizwan Sattar on 10/28/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LKUtils : NSObject
+
++ (CGSize) currentWindowSize;
+
+@end
