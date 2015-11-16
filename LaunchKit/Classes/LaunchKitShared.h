8e42a17d364e37a61e2ad2ca449dcd3194eeff93
Rizwan Sattar <rsattar@gmail.com>
2015-11-16 15:15:31 -0800

    Add documentation to LKConfigReady+Refreshed funcs

--- LaunchKit/Classes/LaunchKitShared.h
+++ LaunchKit/Classes/LaunchKitShared.h
@@ -20,7 +20,19 @@ extern BOOL LKConfigBool(NSString *__nonnull key, BOOL defaultValue);
 extern NSInteger LKConfigInteger(NSString *__nonnull key, NSInteger defaultValue);
 extern double LKConfigDouble(NSString *__nonnull key, double defaultValue);
 extern NSString * __nullable LKConfigString(NSString *__nonnull key, NSString *__nullable defaultValue);
+/**
+ * A block to LKConfigReady will get called on the very first
+ * update to the configuration (whether or not the configuration is different
+ * from the previous configuration). This is an easy place to do some "set once"
+ * tasks for your app.
+ */
 extern void LKConfigReady(LKConfigReadyHandler _Nullable readyHandler);
+/**
+ * A block to LKConfigRefreshed will get called on the very first
+ * network retrieval of the configuration (whether or not the configuration is different
+ * from the previous configuration), and all subsequent changes. This is an easy place
+ * to do some global property setting for your app.
+ */
 extern void LKConfigRefreshed(LKConfigRefreshHandler _Nullable refreshHandler);
 
 #pragma mark - LKAppUser Convenience Functions
