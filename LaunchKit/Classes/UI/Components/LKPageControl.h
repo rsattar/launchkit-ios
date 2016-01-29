1374b5c0be008586139db8315887b6b85338bd0f
Rizwan Sattar <rsattar@gmail.com>
2016-01-29 11:52:53 -0800

    Add basic LKPageControl, support for `vertical`

---
+++ LaunchKit/Classes/UI/Components/LKPageControl.h
@@ -0,0 +1,16 @@
+//
+//  LKPageControl.h
+//  Pods
+//
+//  Created by Rizwan Sattar on 1/29/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+IB_DESIGNABLE
+@interface LKPageControl : UIPageControl
+
+@property (assign, nonatomic) IBInspectable BOOL vertical;
+
+@end
---
+++ LaunchKit/Classes/UI/Components/LKPageControl.m
@@ -0,0 +1,27 @@
+//
+//  LKPageControl.m
+//  Pods
+//
+//  Created by Rizwan Sattar on 1/29/16.
+//
+//
+
+#import "LKPageControl.h"
+
+@implementation LKPageControl
+
+- (void) setVertical:(BOOL)vertical
+{
+    if (_vertical == vertical) {
+        return;
+    }
+    _vertical = vertical;
+    if (_vertical) {
+        self.transform = CGAffineTransformMakeRotation(M_PI / 2.0);
+    } else {
+        self.transform = CGAffineTransformIdentity;
+    }
+    [self setNeedsDisplay];
+}
+
+@end
