c8a40ed749fe3622f0da8bc01eba1cf8c5b1ed8d
Rizwan Sattar <rsattar@gmail.com>
2016-04-04 12:11:24 -0700

    Remove no-longer-used `showUIWithName` call

--- DevSwift/LaunchKitSampleSwift/RootViewController.swift
+++ DevSwift/LaunchKitSampleSwift/RootViewController.swift
@@ -41,36 +41,11 @@ class RootViewController: UIViewController {
                 print("App review card finished with flow result: \(NSStringFromViewControllerFlowResult(flowResult))")
             }
         }
-
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func showUIWithName(uiName:String) {
-        LaunchKit.sharedInstance().uiManager.loadRemoteUIWithId(uiName, completion: { [unowned self] (viewController, error) -> Void in
-            if let viewController = viewController {
-                LaunchKit.sharedInstance().presentRemoteUIViewController(viewController, fromViewController: self, animated: true, dismissalHandler: nil);
-            } else {
-                let message: String
-                if let error = error {
-                    let reason: String
-                    if let errorMessage = error.userInfo["message"] as? String {
-                        reason = errorMessage
-                    } else {
-                        reason = ""
-                    }
-                    message = "Could not load UI named '\(uiName)'.\n\nError \(error.code) - \(reason)"
-                } else {
-                    message = "Could not load UI named '\(uiName)'."
-                }
-                let alertController = UIAlertController(title: "UI not found", message: message, preferredStyle: .Alert);
-                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
-                self.presentViewController(alertController, animated: true, completion: nil)
-            }
-        })
     }
 
     @IBAction func onShowAppReleaseNotesButtonTriggered(sender: UIButton) {
